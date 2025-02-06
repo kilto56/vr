732dffd3401251115e26822bc75862785429df00
kilto56 <112411444+kilto56@users.noreply.github.com>
2025-02-06 17:25:52 +0100

    Add files via upload

---
+++ person.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        Person Sarah = new Person("Sarah", "Croche", 21, "Femme");
+        Person Marc = new Person("Marc", "Eurindelebile", 35, "Homme");
+        
+        string AgeDeSarah = Sarah.GetInfo("Age");
+        Console.WriteLine($"Sarah a {AgeDeSarah}.");
+        
+        Marc.Birthday();
+        
+        List<string> InfoDeSarah = Sarah.GetAllInfo();
+        
+        for (int i = 0; i < InfoDeSarah.Count; i++) {
+            Console.WriteLine(InfoDeSarah[i]);
+        }
+    }
+}
+
+public class Person {
+    public static int NombreDePersonne { get; set; } = 0;
+    public static List<Person> Personnes { get; } = new List<Person>();
+    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+    
+    public Person(string fn, string ln, int a, string g) {
+        NombreDePersonne += 1;
+        
+        FirstName = fn;
+        LastName = ln;
+        Age = a;
+        Gender = g;
+        
+        Personnes.Add(this);
+    }
+    
+    public void Hello() {
+        Console.WriteLine($"{this.FirstName} {this.LastName} says hello !");
+    }
+    
+    public string GetInfo(string info) {
+        if (info == "FirstName") {
+            return this.FirstName;
+        } else if (info == "LastName") {
+            return this.LastName;
+        } else if (info == "Age") {
+            return this.Age.ToString();
+        } else if (info == "Gender") {
+            return this.Gender;
+        } else {
+            return $"No information named {info} !";
+        }
+    }
+    
+    public List<string> Infos;
+    
+    public List<string> GetAllInfo() {
+        Infos = new List<string> {this.FirstName, this.LastName, this.Age.ToString(), this.Gender};
+        return Infos;
+    }
+    
+    public void Birthday() {
+        this.Age += 1;
+        
+        Console.WriteLine($"Bon anniversaire {this.FirstName} {this.LastName} !! Tu as maintenant {this.Age.ToString()} !!");
+    }
+}
